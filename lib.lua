e2ae06c64e82132153818aa4f6420d5b6e4ad4ad
Henrique Barcia Lang <henrique.barcia@gec.inatel.br>
2019-12-09 18:30:39 -0300

    Draw pieces

--- lib.lua
+++ lib.lua
@@ -7,6 +7,12 @@ PAWN_SIZE = 300
 PAWN_SCALE = 0.25
 PAWN_DISPLAY_PAD = 10
 
+PLAYER_ONE = 1
+PLAYER_TWO = 2
+
+PAWN = 1
+CHECKER = 2
+
 -- Get x, y coordinates in hypothetical board based on window coordinates
 function getEquivalentPosition(x, y)
     return math.ceil(x / SQUARE_SIZE), math.ceil(y / SQUARE_SIZE)
--- main.lua
+++ main.lua
@@ -1,23 +1,67 @@
 require("lib")
 
+board = {}
+
 boardFile = nil
 pawnFile = nil
 
-whitePawn = nil
-blackPawn = nil
+pawnIcons = {{}, {}}
 
 function love.load()
     boardFile = love.graphics.newImage("board.png")
     pawnFile = love.graphics.newImage("pawns.png")
 
-    whitePawn = love.graphics.newQuad(4, 4, PAWN_SIZE, PAWN_SIZE, pawnFile:getDimensions())
-    blackPawn = love.graphics.newQuad(314, 4, PAWN_SIZE, PAWN_SIZE, pawnFile:getDimensions())
+    pawnIcons[PLAYER_ONE][PAWN] = love.graphics.newQuad(4, 4, PAWN_SIZE, PAWN_SIZE, pawnFile:getDimensions())
+    pawnIcons[PLAYER_TWO][PAWN] = love.graphics.newQuad(314, 4, PAWN_SIZE, PAWN_SIZE, pawnFile:getDimensions())
+
+    for i = 1, 8 do
+        board[i] = {}
+    end
+
+    local wPawnCount = 1
+    local bPawnCount = 1
+
+    -- Setup board
+    for i = 1, 8, 2 do
+        board[1][i] = { owner = PLAYER_ONE, id = wPawnCount, class = PAWN }
+        wPawnCount = wPawnCount + 1
+
+        board[3][i] = { owner = PLAYER_ONE, id = wPawnCount, class = PAWN }
+        wPawnCount = wPawnCount + 1
+
+        board[7][i] =  { owner = PLAYER_TWO, id = bPawnCount, class = PAWN }
+        bPawnCount = bPawnCount + 1
+    end
+
+    for i = 2, 8, 2 do
+        board[2][i] = { owner = PLAYER_ONE, id = wPawnCount, class = PAWN }
+        wPawnCount = wPawnCount + 1
+
+        board[6][i] =  { owner = PLAYER_TWO, id = bPawnCount, class = PAWN }
+        bPawnCount = bPawnCount + 1
+
+        board[8][i] =  { owner = PLAYER_TWO, id = bPawnCount, class = PAWN }
+        bPawnCount = bPawnCount + 1
+    end
 end
 
 function love.draw()
+    -- Draw board
     love.graphics.draw(boardFile, 0, 0)
-    love.graphics.draw(pawnFile, whitePawn, PAWN_DISPLAY_PAD, PAWN_DISPLAY_PAD, 0, PAWN_SCALE, PAWN_SCALE)
-    love.graphics.draw(pawnFile, blackPawn, 96 + PAWN_DISPLAY_PAD, PAWN_DISPLAY_PAD, 0, PAWN_SCALE, PAWN_SCALE)
+    
+    -- Draw pieces
+    for i = 0, 7 do
+        for j = 0, 7 do
+            local piece = board[i + 1][j + 1]
+
+            if piece then
+                local icon = pawnIcons[piece.owner][piece.class]
+
+                love.graphics.draw(pawnFile, icon, (j * SQUARE_SIZE) + PAWN_DISPLAY_PAD, (i * SQUARE_SIZE) + PAWN_DISPLAY_PAD, 0, PAWN_SCALE, PAWN_SCALE)
+            end
+        end
+    end
+
 end
 
 function love.mousereleased(x, y, button, istouch)
